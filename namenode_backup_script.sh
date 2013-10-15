6f7c39d55ec5e8a576ac2818af15595e6a64ffe6
gubs <gubs4u@gmail.com>
2013-10-15 15:22:07 -0400

    first commit

---
+++ namenode_backup_script.sh
@@ -0,0 +1,19 @@
+nameNode=devnamenode
+timeStamp=`date +%Y-%m-%d-%H-%M`
+twoDayAgo=`date +%Y-%m-%d --date="1 day ago"`
+backupdir=/mysql/dump/namenode_backup
+workDir=${backupdir}/work
+
+curl -s http://${nameNode}:50070/getimage?getimage=1 > $workDir/fsimage
+curl -s http://${nameNode}:50070/getimage?getedit=1 > $workDir/edits
+
+cd $backupdir
+
+tar -czf namenode_backup_${timeStamp}.tar.gz work/
+
+#Delete download files
+rm -f $workDir/edits
+rm -f $workDir/fsimage
+
+#Delete old files
+rm -fr namenode_backup_${twoDayAgo}*.tar.gz
